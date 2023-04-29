c169e7edd807dbe76644930fbf944f65c009b719
nourahassan <nourahassan279@gmail.com>
2023-04-29 18:33:09 +0300

    pages

--- pages/404/index.vue
+++ pages/404/index.vue
@@ -1,11 +1,11 @@
 <template>
-<div class="container" style="min-height:300px; margin-top: 100px;">
+<div class="container" style="min-height:400px; margin-top: 100px;">
         <div class="row">
           <div class="col-md-6 offset-md-3 text-center">
             <h1>404</h1>
             <p>{{$t("main_page.error")}}</p>
 				<NuxtLink :to="localePath('/')" class="btn btn-primary">
-				{{$t("main_page.go_to_homepage")}}
+				   {{$t("main_page.go_to_homepage")}}
 				</NuxtLink>
           </div>
         </div>
--- pages/products/_id/index.vue
+++ pages/products/_id/index.vue
@@ -8,40 +8,48 @@
     >
     <section class="section row-middle">
 	<div class="container mb-5">
-			<div class="row">
-			<div class="col-md-4">
-				<img :src="singleProduct.vProductImage" class="img-fluid" alt="Product Image">
-			</div>
-			<div :class="$i18n.locale == 'en'? 'text-left col-md-8' : 'text-right col-md-8'">
-				<h1 class="mb-4 product-color">{{singleProduct.vProductName}} {{singleProduct.vProductUnit}}</h1>
-				<h2 class="mb-4">
-				<s style="font-size: 1.5rem" class="text-danger"
-				      v-if="singleProduct.dDiscountedProductPrice !=singleProduct.dbOriginalProductPrice">
-                 {{ singleProduct.dbOriginalProductPrice }} </s>
-				   <span class="product-color" style="font-size: 2rem">
-                        {{ singleProduct.dDiscountedProductPrice }}
-                        {{ $t('main_page.sar') }}</span>
-                </h2>
-				<p class="mb-4">{{singleProduct.txProductDescription}}</p>
+			<div class="row" v-if="isExist">
+				<div class="col-md-4">
+					<img :src="singleProduct.vProductImage" class="img-fluid" alt="Product Image">
+				</div>
+				<div :class="$i18n.locale == 'en'? 'text-left col-md-8' : 'text-right col-md-8'">
+					<h1 class="mb-4 product-color">{{singleProduct.vProductName}} {{singleProduct.vProductUnit}}</h1>
+					<h2 class="mb-4">
+					<s style="font-size: 1.5rem" class="text-danger"
+						v-if="singleProduct.dDiscountedProductPrice !=singleProduct.dbOriginalProductPrice">
+					{{ singleProduct.dbOriginalProductPrice }} </s>
+					<span class="product-color" style="font-size: 2rem">
+							{{ singleProduct.dDiscountedProductPrice }}
+							{{ $t('main_page.sar') }}</span>
+					</h2>
+					<p class="mb-4">{{singleProduct.txProductDescription}}</p>
 
-              <div class="btn-test">
-				<div class="">
-				  <div class="location1 d-flex">
-					<span class=" subheading" style="margin-right: 10px;;">
-					  <a href="https://apps.apple.com/us/app/%D9%85%D8%AA%D9%8A%D8%B1%D9%8A%D8%A7%D9%84/id1626095299" target="_blank">
-						 <img src="~/assets/img/appstore.svg" alt="" class="test-10"  style="margin-bottom: 10px; " /></a>
-					</span>
-					<span class="mb-3 subheading" style="margin-right: 10px">
-					  <a href="https://play.google.com/store/apps/details?id=com.material.customer" target="_blank">
-						<img class="test-10" src="~/assets/img/googleplay.svg"  alt=""/></a>
-					</span>
-				  </div>
+				<div class="btn-test">
+					<div class="">
+					<div class="location1 d-flex">
+						<span class=" subheading" style="margin-right: 10px;;">
+						<a href="https://apps.apple.com/us/app/%D9%85%D8%AA%D9%8A%D8%B1%D9%8A%D8%A7%D9%84/id1626095299" target="_blank">
+							<img src="~/assets/img/appstore.svg" alt="" class="test-10"  style="margin-bottom: 10px; " /></a>
+						</span>
+						<span class="mb-3 subheading" style="margin-right: 10px">
+						<a href="https://play.google.com/store/apps/details?id=com.material.customer" target="_blank">
+							<img class="test-10" src="~/assets/img/googleplay.svg"  alt=""/></a>
+						</span>
+					</div>
+					</div>
+				</div>
+				</div>
+            </div>
+            <div class="row" v-else>
+				<div class="col-md-6 offset-md-3 text-center">
+					<h4>{{$t("products.not_found")}}</h4>
+						<NuxtLink :to="localePath('/products')" class="btn btn-primary">
+						{{$t("products.go_to_products")}}
+						</NuxtLink>
 				</div>
 			</div>
-			</div>
-
 		
-		</div>
+		
 		</div>
 	</section>  
 	</b-overlay>
@@ -58,13 +66,16 @@ export default {
     return {
 		singleProduct:Object,
 		 isLoaded: false,
+		 isExist :true,
     }
   },
   methods: {
     async getSingleProduct() {
       const productId =this.$route.params?.id
       const res = await Product_API.getProductDetail(productId)
-      this.singleProduct = res.data.responseData?.productDetails;
+	  this.isExist = res.data.responseCode == 200 ? true : false
+	  if(this.isExist)
+        this.singleProduct = res.data.responseData?.productDetails;
 	  this.isLoaded = true;
     }
   },
